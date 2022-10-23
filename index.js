6544d0b25ed920ab21bc9f4156e3b987c9cec377
Mike Davis <micdavis93@gmail.com>
2022-10-23 16:41:27 -0600

    commit tree fcbcc1b486b0cf9370f8ff32c186c6967447944a

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.querySelector("body main#main");
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = "Mike Davis is the champion";
